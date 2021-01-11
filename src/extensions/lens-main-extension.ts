17a6b366f4d362219b57dc155a6ce7324eb8f1f0
Sebastian Malton <sebastian@malton.name>
2021-01-11 16:33:19 -0500

    Add lens:// protocol handling with a routing mechanism
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/extensions/lens-main-extension.ts
+++ src/extensions/lens-main-extension.ts
@@ -2,6 +2,8 @@ import type { MenuRegistration } from "./registries/menu-registry";
 import { LensExtension } from "./lens-extension";
 import { WindowManager } from "../main/window-manager";
 import { getExtensionPageUrl } from "./registries/page-registry";
+import { RouteHandler } from "../common/protocol-handler";
+import { LensProtocolRouterMain } from "../main/protocol-handler";
 
 export class LensMainExtension extends LensExtension {
   appMenus: MenuRegistration[] = [];
@@ -15,5 +17,22 @@ export class LensMainExtension extends LensExtension {
     });
 
     await windowManager.navigate(pageUrl, frameId);
+  }
+
+  /**
+   * Registers a handler to be called when a `lens://` link is called.
+   *
+   * See https://www.npmjs.com/package/path-to-regexp. To use this the link
+   * `lens://extensions/<org-id>/<extension-name>/your/defined/path?with=query`
+   * or `lens://extensions/<extension-name>/your/defined/path?with=query`
+   * (if this extension is not packaged behind an organization) needs to be
+   * opened.
+   * @param pathSchema The path schema for the route.
+   * @param handler The function to call when this route has been matched
+   */
+  onProtocol(pathSchema: string, handler: RouteHandler): void {
+    const lprm = LensProtocolRouterMain.getInstance<LensProtocolRouterMain>();
+
+    lprm.extensionOn(this.name, pathSchema, handler);
   }
 }
